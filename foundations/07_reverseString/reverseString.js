c2615f656ba4269b37e3e7ce8f0edb9032e9feea
hardfinetry-whitey <221923048+hardfinetry@users.noreply.github.com>
2026-04-05 16:30:01 +0800

    solve 07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringLength = string.length;
+    let reversed = "";
+    for (let i = stringLength -1;
+        i >= 0;
+        i--) {
+            reversed += string[i];
+        }
+    return reversed
 };
 
 // Do not edit below this line
